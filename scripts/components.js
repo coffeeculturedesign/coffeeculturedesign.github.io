e8326102ac87cf9e0857dc084cd28e773b2986f7
coffeeculturedesign <crolalibe@gmail.com>
2024-08-23 14:49:00 +0200

    seo

--- scripts/components.js
+++ scripts/components.js
@@ -1,7 +1,7 @@
 function getNavBar(where) {
     let component = /* html */
         `<div class="brand-name">
-        <a href="../"><h1>Coffee Culture</h1></a>
+        <h1 onclick="location.replace('../')">Coffee Culture</h1>
     </div>
     <div class="close-menu" id="close-menu">
         <svg width="35" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
